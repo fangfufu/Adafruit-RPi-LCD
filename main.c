6abec387fbfc97aef6cc676725fe827d177f09c0
Fufu Fang <fangfufu@fangfufu.co.uk>
2013-09-02 08:04:54 +0800

    somehow the --verbose flag stops set_colour from running

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <getopt.h>
 
@@ -8,30 +9,34 @@
 #include "lcd.h"
 #include "lcd_cgram.h"
 
-static void set_colour(const char* s);
-static void set_cursor(const char* s);
-static void help();
-static void lcd_off();
+static int set_colour(const char* s);
+static int set_cursor(const char* s);
+static int help();
 
-int verbose_flag = 0;
-int lcd_off_flag = 0;
-uint8_t lcd_opts = 0;
+static int lcd_off = 0;
+static uint8_t lcd_opts = 0;
+static int verbose = 0;
 
 int main(int argc, char* const* argv)
 {
     int s;
     int opts_index = 0;
 
-    char* short_opts = "hc:u:";
+    char* short_opts = "hc:u:v";
     struct option long_opts[] = {
+        {"off", no_argument, &lcd_off, 1},
+        {"verbose", no_argument, &verbose, 1},
         {"help", no_argument, NULL, 'h'},
-        {"led", required_argument, NULL, 'l'},
+        {"colour", required_argument, NULL, 'c'},
         {"cursor", required_argument, NULL, 'u'},
-        {"off", no_argument, &lcd_off_flag, 1},
-        {"verbose", no_argument, &verbose_flag, 1},
         {0, 0, 0, 0}
     };
 
+    GPIO_open();
+    LCD_init(0);
+
+    int option_index;
+    
     while( (s = getopt_long(
         argc, argv, short_opts, long_opts, &opts_index)) > 0 ) {
         switch(s){
@@ -39,7 +44,7 @@ int main(int argc, char* const* argv)
                 printf("help\n");
                 break;
             case 'c':
-                printf("colour: %s\n", optarg);
+                set_colour(optarg);
                 break;
             case 'u':
                 printf("cursor: %s\n", optarg);
@@ -49,11 +54,10 @@ int main(int argc, char* const* argv)
         }
     };
 
-    GPIO_open();
-    LCD_init(lcd_opts);
+    printf("main verbose: %d\n", verbose);
 
-    if(lcd_off_flag){
-        if(verbose_flag){
+    if(lcd_off){
+        if(verbose){
             printf("Turning off LCD\n");
         }
         LCD_colour(Black);
@@ -64,4 +68,36 @@ int main(int argc, char* const* argv)
         LCD_wrap_printf("%s ", argv[optind++]);
     }
     return 0;
+}
+
+int set_colour(const char* carg)
+{
+    printf("set_colour verbose: %d\n", verbose);
+
+    Colour c = Black;
+
+    if(!strncasecmp(carg, "White", 5)) {
+        c = Black;
+    } else if (!strncasecmp(carg, "Red", 3)) {
+        c =  Red;
+    } else if (!strncasecmp(carg, "Yellow", 6)) {
+        c =  Yellow;
+    } else if (!strncasecmp(carg, "Green", 5)) {
+        c =  Green;
+    } else if (!strncasecmp(carg, "Cyan", 4)) {
+        c =  Cyan;
+    } else if (!strncasecmp(carg, "Blue", 4)) {
+        c =  Blue;
+    } else if (!strncasecmp(carg, "Magenta", 7)) {
+        c =  Magenta;
+    } else if (!strncasecmp(carg, "White", 5)) {
+        c =  White;
+    } else {
+        fprintf(stderr, "string_to_colour error: Invalid colour");
+    }
+
+    if(verbose) {
+        printf("Setting LCD colour to: %s\n", carg);
+    }
+    return LCD_colour(c);
 }
